eb72a020dd63fa69bcf8e8a72a492a7e44fc4eef
makun <michaelmakun@aliyun.com>
2017-02-20 21:31:16 +0800

    add db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,17 @@ u.is_admin = true
 u.save
 
 puts "Created admin account"
+
+products = [{title:"1号造型师作品",description:"擅长女装",image_path: "#{Rails.root}/app/assets/images/woman.jpg"},
+            {title:"2号造型师作品",description:"擅长男装",image_path: "#{Rails.root}/app/assets/images/men.jpeg"},
+            {title:"3号造型师作品",description:"业界大牛",image_path: "#{Rails.root}/app/assets/images/niu.jpg"},
+            {title:"4号造型师作品",description:"极具美感",image_path: "#{Rails.root}/app/assets/images/beauty.jpg"}
+          ]
+
+p1 = Product.create([title:products[0][:title],description:products[0][:description],quantity:99,price:400,image:open(products[0][:image_path])])
+p2 = Product.create([title:products[1][:title],description:products[1][:description],quantity:99,price:600,image:open(products[1][:image_path])])
+p3 = Product.create([title:products[2][:title],description:products[2][:description],quantity:99,price:500,image:open(products[2][:image_path])])
+p4 = Product.create([title:products[3][:title],description:products[3][:description],quantity:99,price:700,image:open(products[3][:image_path])])
+
+
+puts "products created."
