71666856c08a00185f3a1e704b387c67f6520011
makun <michaelmakun@aliyun.com>
2017-02-21 21:21:35 +0800

    change carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,8 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.fog_provider = 'fog'
+    config.storage  :fog
+    #config.fog_provider = 'fog'
     config.fog_credentials = {
       provider:       'AWS',
       aws_access_key_id:   ENV["AWS_ACCESS_KEY_ID"],
